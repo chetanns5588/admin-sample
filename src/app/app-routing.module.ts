60d258e889fa9f99aca57b3566546362d47c0d69
Chetan N S <chetanns5588@gmail.com>
2020-11-04 13:22:51 +0530

    Routing Issue

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './user-pages/login/login.component';
+import { RegisterComponent } from './user-pages/register/register.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'basic-ui', loadChildren: () => import('./basic-ui/basic-ui.module').then(m => m.BasicUiModule) },
+  { path: 'charts', loadChildren: () => import('./charts/charts.module').then(m => m.ChartsDemoModule) },
+  { path: 'forms', loadChildren: () => import('./forms/form.module').then(m => m.FormModule) },
+  { path: 'tables', loadChildren: () => import('./tables/tables.module').then(m => m.TablesModule) },
+  { path: 'icons', loadChildren: () => import('./icons/icons.module').then(m => m.IconsModule) },
+  { path: 'general-pages', loadChildren: () => import('./general-pages/general-pages.module').then(m => m.GeneralPagesModule) },
+  { path: 'apps', loadChildren: () => import('./apps/apps.module').then(m => m.AppsModule) },
+  { path: 'error-pages', loadChildren: () => import('./error-pages/error-pages.module').then(m => m.ErrorPagesModule) },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
